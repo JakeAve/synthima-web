3eed78b54eec454eee70099453c95aae953a9509
JakeAve <jacobjavery@gmail.com>
2025-02-13 15:39:22 -0700

    Added a small icon switch

--- islands/Passwords.tsx
+++ islands/Passwords.tsx
@@ -25,7 +25,9 @@ export default function Passwords(props: Props) {
                 selection.removeAllRanges();
               }
 
-              e.currentTarget.nextElementSibling?.childNodes.forEach(
+              const btn = e.currentTarget.closest("button");
+
+              btn?.nextElementSibling?.childNodes.forEach(
                 (n) => {
                   const range = document.createRange();
                   range.selectNode(n);
@@ -34,6 +36,18 @@ export default function Passwords(props: Props) {
               );
 
               navigator.clipboard.writeText(v);
+
+              if (btn) {
+                btn.innerHTML =
+                  '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg>';
+              }
+
+              setTimeout(() => {
+                if (btn) {
+                  btn.innerHTML =
+                    '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="M360-240q-33 0-56.5-23.5T280-320v-480q0-33 23.5-56.5T360-880h360q33 0 56.5 23.5T800-800v480q0 33-23.5 56.5T720-240H360Zm0-80h360v-480H360v480ZM200-80q-33 0-56.5-23.5T120-160v-560h80v560h440v80H200Zm160-240v-480 480Z"/></svg>';
+                }
+              }, 3000);
             }}
           >
             <svg
