e62472323481f65a0b48c3e81c9fce2a5c6c59c4
JakeAve <jacobjavery@gmail.com>
2026-05-24 15:09:39 -0600

    update logo

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -6,6 +6,8 @@ export default function App({ Component }: PageProps) {
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Synthima</title>
+        <link rel="icon" href="/logo.svg" type="image/svg+xml" />
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
       </head>
       <body class="bg-neutral-50 dark:bg-neutral-800">
         <Component />
